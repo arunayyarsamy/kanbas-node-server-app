10ec4cdcd63763abf87a6cbe188244bd30b4f434
Arun Ayyarsamy <ayyarsamy.a@northeastern.edu>
2024-04-04 22:04:15 -0400

    Assignment 5 server changes

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    "_id": "RS101",
+    "name": "Rocket Propulsion",
+    "number": "RS101",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "propulsion.jpg"
+  },
+  {
+    "_id": "RS102",
+    "name": "Aero Dynamics",
+    "number": "RS102",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "rocket.jpeg"
+  },
+  {
+    "_id": "RS103",
+    "name": "Spacecraft Structural Design",
+    "number": "RS103",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "spacecraft.jpg"
+  },
+  {
+    "_id": "RS104",
+    "name": "Spacecraft Propulsion",
+    "number": "RS104",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "propulsion.jpg"
+  },
+  {
+    "_id": "RS105",
+    "name": "Spacecraft Dynamics",
+    "number": "RS105",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "rocket.jpeg"
+  },
+  {
+    "_id": "RS106",
+    "name": "Spacecraft Control",
+    "number": "RS106",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "spacecraft.jpg"
+  },
+  {
+    "_id": "RS107",
+    "name": "Spacecraft Communication",
+    "number": "RS107",
+    "startDate": "2019-01-01",
+    "endDate": "2019-12-31",
+    "image": "rocket.jpeg"
+  }
+]
